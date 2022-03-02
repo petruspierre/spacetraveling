d47426798f8d7e708a33fa8bfe7c8bacfe80a842
Petrus Bento <petrus.tsx@gmail.com>
2022-03-01 21:00:07 -0300

    Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import styles from './header.module.scss';
+
 export default function Header() {
   return (
-    <div>
+    <div className={styles.container}>
       <img src="Logo.svg" alt="logo" />
     </div>
   );
